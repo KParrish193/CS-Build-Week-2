57e3600686d43f3ed73af9b5ed79fbd4cda94320
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-06-24 14:32:45 -0600

    twoSum attempt 1

---
+++ leetcode challenges/twoSum.py
@@ -0,0 +1,31 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# Example:
+
+# Given nums = [2, 7, 11, 15], target = 9,
+
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        indices = []
+        
+# loop through nums list
+        for i in nums:
+# if target - i = another element in the nums list, append [i] to indices list 
+            if target - i == (something):
+                indices.append(index-of-i)
+                indices.append(index-of-other-element)
+                return indices
+# condition isn't met with any of the elements, return None
+            else:
+                return None
