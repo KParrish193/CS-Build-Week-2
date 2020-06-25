39af2cbb7f15cf7ab1da19da660c9746b648c45d
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-06-25 15:35:41 -0600

    find duplicate and mergeTwoLists added

--- leetcode challenges/twoSum.py
+++ leetcode challenges/twoSum.py
@@ -16,12 +16,16 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-
+        
+        indices = []
+        
 # loop through nums list
-        for sum1 in range(len(nums)):
-            for sum2 in range(sum1+1, len(nums)):
-                if nums[sum1] + nums[sum2] == target:
-                    return [sum1, sum2]
-        # condition isn't met with any of the elements, return None
-                else:
-                    return None
+        for i in range(len(nums)):
+# if target - i = another element in the nums list, append [i] to indices list 
+            if target - i == (something):
+                indices.append(index-of-i)
+                indices.append(index-of-other-element)
+                return indices
+# condition isn't met with any of the elements, return None
+            else:
+                return None
